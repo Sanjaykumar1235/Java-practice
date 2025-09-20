93fc65c3670ecf1248f3837b376aa57cf4af9a77
Sanjaykumar1235 <sanjubai1235@gmail.com>
2025-09-20 13:33:15 +0530

    Java

---
+++ Name.java
@@ -0,0 +1,12 @@
+class Name
+{
+    public static void main(String [] args){
+
+        int count =1;
+
+        while(count<=10){
+            System.out.println("Sanjay");
+            count ++;
+        }
+    }
+}
